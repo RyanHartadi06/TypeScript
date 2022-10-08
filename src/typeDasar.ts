7f852bb4b1b813f64512d80fb60120144de35e60
Ryan Hartadi <Ryanhartadi06@gmail.com>
2022-10-08 17:46:14 +0700

    Update

---
+++ src/typeDasar.ts
@@ -0,0 +1,19 @@
+// string
+let nama: string = "Ryan Hartadi";
+nama = "asd";
+
+console.log(nama);
+
+// number
+
+let number: number;
+
+number = 30;
+
+// boolean
+
+let isMarried: boolean;
+
+isMarried = false;
+
+console.log(isMarried);
